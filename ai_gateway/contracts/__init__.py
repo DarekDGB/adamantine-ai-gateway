c38773f0bc0b98f235282e77c397255f4a3ec833
DarekDGB <majescyk@gmail.com>
2026-03-28 14:59:43 +0000

    build(contracts): expose envelope v1 contract symbols

---
+++ ai_gateway/contracts/__init__.py
@@ -0,0 +1,9 @@
+from ai_gateway.contracts.envelope_v1 import (
+    AI_GATEWAY_ENVELOPE_V1,
+    REQUIRED_ENVELOPE_FIELDS,
+)
+
+__all__ = [
+    "AI_GATEWAY_ENVELOPE_V1",
+    "REQUIRED_ENVELOPE_FIELDS",
+]
