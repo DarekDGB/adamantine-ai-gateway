a2d594dd6ba5bca052584e47339c2cc5a8024702
DarekDGB <majescyk@gmail.com>
2026-03-30 21:16:53 +0100

    feat: expose gateway, registry and receipt builder as public api

--- ai_gateway/__init__.py
+++ ai_gateway/__init__.py
@@ -1,3 +1,11 @@
+from ai_gateway.gateway import AIGateway
+from ai_gateway.receipt import build_receipt_v1
+from ai_gateway.registry import AdapterRegistry
 from ai_gateway.version import __version__
 
-__all__ = ["__version__"]
+__all__ = [
+    "__version__",
+    "AIGateway",
+    "AdapterRegistry",
+    "build_receipt_v1",
+]
