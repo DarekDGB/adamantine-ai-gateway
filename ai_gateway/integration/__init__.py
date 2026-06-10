faf9f520b2943fc9b1634586fc0837bc7d8b6986
DarekDGB <majescyk@gmail.com>
2026-06-10 16:33:31 +0100

    Initialize integration module with Adamantine imports

---
+++ ai_gateway/integration/__init__.py
@@ -0,0 +1,11 @@
+from ai_gateway.integration.adamantine import (
+    ADAMANTINE_AI_GATEWAY_EVIDENCE_V1,
+    build_adamantine_ai_gateway_evidence_from_gateway_result_v1,
+    build_adamantine_ai_gateway_evidence_v1,
+)
+
+__all__ = [
+    "ADAMANTINE_AI_GATEWAY_EVIDENCE_V1",
+    "build_adamantine_ai_gateway_evidence_from_gateway_result_v1",
+    "build_adamantine_ai_gateway_evidence_v1",
+]
