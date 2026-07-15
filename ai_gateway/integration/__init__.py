67908b3c52063f9c79ad385dd3f4aaa39b75839b
DarekDGB <majescyk@gmail.com>
2026-07-15 18:34:50 +0100

    Add ADAMANTINE_AI_GATEWAY_EVIDENCE_V2 to integration

--- ai_gateway/integration/__init__.py
+++ ai_gateway/integration/__init__.py
@@ -1,11 +1,17 @@
 from ai_gateway.integration.adamantine import (
     ADAMANTINE_AI_GATEWAY_EVIDENCE_V1,
+    ADAMANTINE_AI_GATEWAY_EVIDENCE_V2,
     build_adamantine_ai_gateway_evidence_from_gateway_result_v1,
+    build_adamantine_ai_gateway_evidence_from_gateway_result_v2,
     build_adamantine_ai_gateway_evidence_v1,
+    build_adamantine_ai_gateway_evidence_v2,
 )
 
 __all__ = [
     "ADAMANTINE_AI_GATEWAY_EVIDENCE_V1",
+    "ADAMANTINE_AI_GATEWAY_EVIDENCE_V2",
     "build_adamantine_ai_gateway_evidence_from_gateway_result_v1",
+    "build_adamantine_ai_gateway_evidence_from_gateway_result_v2",
     "build_adamantine_ai_gateway_evidence_v1",
+    "build_adamantine_ai_gateway_evidence_v2",
 ]
